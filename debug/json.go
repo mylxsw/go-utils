aaeba79dd56546c07207d3e91cb092a5e4b0702f
管宜尧 <mylxsw@aicode.cc>
2021-03-07 16:32:13 +0800

    add debug package

---
+++ debug/json.go
@@ -0,0 +1,17 @@
+package debug
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// MustMarshalJSON 将对象编码为 json， 出错时返回字符串 对象解析失败
+// 注意：仅在调试时使用
+func MustMarshalJSON(obj interface{}) string {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return fmt.Sprintf("parse object to json failed: %v", err)
+	}
+
+	return string(data)
+}
